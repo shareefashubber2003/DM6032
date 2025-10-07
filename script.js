295099f5f0437c0c64c3a6f83ca0575558139f14
shareefashubber2003 <shareefashubber40@gmail.com>
2025-10-07 15:38:59 +0300

    Update script.js

--- script.js
+++ script.js
@@ -2,13 +2,13 @@ document.getElementById("contactForm").onsubmit = function () {
   alert("Your inquiry has been submitted. "); return false;
 };
 
-document.getElementByTagName("tr")[1].onclick = function () {
+document.getElementsByTagName("tr")[1].onclick = function () {
   alert("Monday class at 10 - 11 PM in room 0000");
   
 };
 
 
-document.getElementByTagName("tr")[2]. onclick = function () {
+document.getElementsByTagName("tr")[2]. onclick = function () {
   alert("Thursday class at 12- 1PM in room 0000");
   
   
